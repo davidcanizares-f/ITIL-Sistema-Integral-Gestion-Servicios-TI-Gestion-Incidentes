bf6f79239cda611a005fee51fade1e8b8ab08dd1
David Canizares <scaniizares@gmail.com>
2026-07-06 21:16:46 -0500

    Etiqueta inicial

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ =============== UNIVERSIDAD DE LAS AMÉRICAS ===============
+        FACULTAD DE INGENIERÍA Y CIENCIAS APLICADAS
+                INGENIERÍA DE SOFTWARE
+                    PROGRAMACIÓN I
+Autores:
+    -David Cañizares
+    -Sebastián Orbe
+    -Jean Pierre Rosas
+    -Derek Yépez
+
+=============== SISTEMA INTEGRAL DE GESTIÓN DE SERVICIOS TI =============
+                          GESTIÓN DE INCIDENTES
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>    INCIDEX   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+*/
